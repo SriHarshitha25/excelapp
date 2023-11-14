8504bb7c3a71acf279208e4bda81db23f27fb6b4
SriHarshitha25 <85791636+SriHarshitha25@users.noreply.github.com>
2023-11-14 08:37:40 +0530

    Add files via upload

--- savingtoex.py
+++ savingtoex.py
@@ -1,14 +1,7 @@
 import tkinter as tk
 from tkinter import Entry, ttk
-import os
-import openpyxl 
-from openpyxl.chart import BarChart3D,Reference
-from pandas import DataFrame
-from tkinter import messagebox
-import openpyxl as py
-from openpyxl.chart import BarChart,Reference
-from tkcalendar import DateEntry
-from openpyxl import load_workbook
+import openpyxl
+from openpyxl import load_workbook, Workbook
 from tkinter import *
 import tkinter.ttk as ttk
 
@@ -18,6 +11,8 @@ import tkinter.ttk as ttk
 # Load the xlsx file, then store the value of each column in the "elements" list
 wb = load_workbook(filename=r"C:\Users\Chinnu\Downloads\exampleapp\testdata.xlsx")
 ws = wb['Sheet1']
+wBook = load_workbook('storingfile.xlsx')
+sheet = wBook.active
 
 m_row = 1
 m_col=  ws.max_column
@@ -31,19 +26,26 @@ for cell in MaterialDescription:
         elements.append(y)
         print(y)
 
+
 #search function
 def check_input(event):
-    value = event.widget.get()
+    value2 = event.widget.get()
 
-    if value == '':
+    if value2 == '':
         combodata['values'] = elements
     else:
         data = []
         for item in elements:
-            if value.lower() in item.lower():
+            if value2.lower() in item.lower():
                 data.append(item)
 
         combodata['values'] = data
+        
+        print("selected value=", combodata.get())
+        data1 = [combodata.get()]
+        sheet.append(data1)
+        wBook.save('storingfile.xlsx')
+
 
 # Tkinter stuff
 win = Tk()
@@ -55,5 +57,7 @@ combodata = ttk.Combobox(win, values=elements)
 combodata.grid(row=1, column=1, padx=10, pady=10)
 combodata['values'] = elements
 combodata.bind('<KeyRelease>', check_input)
+wBook.save('storingfile.xlsx')
+
 
 win.mainloop()
