1794107c967b76fc1bcbaa68fb9e8c6104279bd3
SriHarshitha25 <85791636+SriHarshitha25@users.noreply.github.com>
2023-11-15 22:14:51 +0530

    Add files via upload

--- savingtoex.py
+++ savingtoex.py
@@ -14,10 +14,12 @@ ws = wb['Sheet1']
 wBook = load_workbook('storingfile.xlsx')
 sheet = wBook.active
 
-m_row = 1
+m_row = ws.max_row
 m_col=  ws.max_column
+print("total rows of the excel: ",m_row)
 MaterialDescription = ws['A2':'A10']
-elements = []       
+elements = []  
+codeval = ''     
 
 #to get the list of column values
 for cell in MaterialDescription:
@@ -40,8 +42,19 @@ def check_input(event):
                 data.append(item)
 
         combodata['values'] = data
+
+        print("selected value=", combodata.get(), "index=",combodata.current())
+
+        main_sheet = wb.active
+        for row in range(2, main_sheet.max_row + 1):
+            material = main_sheet.cell(row=row, column=1).value
+            if material == combodata.get():
+                codeval = main_sheet.cell(row=row, column=2).value
+                print(main_sheet.cell(row=row, column=2).value)
+                code.insert("end-1c", codeval)
+                break         
         
-        print("selected value=", combodata.get())
+
         data1 = [combodata.get()]
         sheet.append(data1)
         wBook.save('storingfile.xlsx')
@@ -59,5 +72,8 @@ combodata['values'] = elements
 combodata.bind('<KeyRelease>', check_input)
 wBook.save('storingfile.xlsx')
 
+ttk.Label(text="Material Code:").grid(row=2, column=0, padx=10, pady=10)
+code = Text(height=1, width=18)
+code.grid(row=2, column=1, padx=10, pady=10)
 
 win.mainloop()
